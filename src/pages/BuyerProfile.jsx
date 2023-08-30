8c996539a29405a1bef9067d7bdd7bfa8f2b960c
Akith-002 <chandinubaa.22@uom.lk>
2023-08-30 13:11:23 +0530

    top container finished

--- src/pages/BuyerProfile.jsx
+++ src/pages/BuyerProfile.jsx
@@ -1,4 +1,12 @@
 import styles from "../styles/BuyerProfile.module.css";
+import {
+  SignOut,
+  Package,
+  Heart,
+  BookmarksSimple,
+  User,
+  Eye,
+} from "@phosphor-icons/react";
 
 const BuyerProfile = () => {
   return (
@@ -12,17 +20,43 @@ const BuyerProfile = () => {
             />
           </div>
           <div className={styles.profileInfo}>
-            <span style={{ fontSize: "2em" }}>Customer's Name</span>
+            <span style={{ fontSize: "2em", lineHeight: "1em" }}>
+              Customer's Name
+            </span>
             <span>Buyer</span>
-            <div className={styles.links}>
-              <button>Sign Out</button>
-              <span>
+            <div className={styles.buttonSet1}>
+              <button>
+                <SignOut size={24} color="#ffffff" weight="duotone" />
+                Sign Out
+              </button>
+              <span style={{ margin: "0em 0.7em" }}>
                 Do you want to sell? <a href="">Create a seller profile</a>
               </span>
             </div>
           </div>
         </div>
-        <div className={styles.iconList}>Icons</div>
+        <div className={styles.iconList}>
+          <span>
+            <Package size={32} weight="duotone" />
+            Orders
+          </span>
+          <span>
+            <Heart size={32} weight="duotone" />
+            Wish List
+          </span>
+          <span>
+            <BookmarksSimple size={32} weight="duotone" />
+            Saved
+          </span>
+          <span>
+            <User size={32} weight="duotone" />
+            Following
+          </span>
+          <span>
+            <Eye size={32} weight="duotone" />
+            Viewed
+          </span>
+        </div>
       </div>
       <div className={styles.recentItems}></div>
       <div className={styles.moreItems}></div>
